67795891d4c8c7082b868b9d3d2ca8afccbc337e
suke3 <35905884+suke3-gh@users.noreply.github.com>
2023-07-06 21:25:53 +0900

    ver.0.0.2

--- a_loudness.ps1
+++ a_loudness.ps1
@@ -1,6 +1,7 @@
 
 Param($format)
-foreach ($fileName in Get-ChildItem -Filter *.${format} ) {
-  ffmpeg -i ${fileName} -hide_banner -filter:a loudnorm=I=-14:LRA=23:TP=-1:print_format=json -vn -f null - 2> loudness-${fileName}.json
-  Get-Content loudness.json -Tail 12 | Tee-Object loudness-${fileName}.json
+foreach ($inputFile in Get-ChildItem -Filter *.${format} ) {
+  $resultFileName = "loudness-${inputFile}.json"
+  ffmpeg -i ${inputFile} -hide_banner -filter:a loudnorm=I=-14:LRA=23:TP=-1:print_format=json -vn -f null - 2> $resultFileName
+  Get-Content $resultFileName -Tail 12 | Tee-Object -FilePath $resultFileName
 }
