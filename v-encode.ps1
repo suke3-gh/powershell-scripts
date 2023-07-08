f9f0662ed8634636332b1d659326461ec99ef922
suke3 <35905884+suke3-gh@users.noreply.github.com>
2023-07-08 23:23:43 +0900

    ver.0.0.4

---
+++ v-encode.ps1
@@ -0,0 +1,21 @@
+
+Param($inputFile)
+
+$loudnessInfo = "loudness-${inputFile}.json"
+
+Write-Host "Checking the loudness..."
+
+ffmpeg -i ${inputFile} -hide_banner -filter:a loudnorm=I=-14:LRA=23:TP=-1:offset=0:print_format=json -vn -f null - 2> $loudnessInfo
+Write-Host "fileName: ${inputFile}"
+Get-Content $loudnessInfo -Tail 12 | Tee-Object -FilePath $loudnessInfo
+
+$jsonData = Get-Content -Raw $loudnessInfo | ConvertFrom-Json
+$audioFilterContent = "loudnorm=I=-14:LRA=23:TP=-1:offset=0:measured_I=" + $jsonData.input_i + ":measured_TP=" + $jsonData.input_tp + ":measured_LRA=" + $jsonData.input_lra + ":measured_thresh=" + $jsonData.input_thresh + ":offset=" + $jsonData.target_offset + ":linear=false,anlmdn=s=0.00001:p=0.002:r=0.002"
+Write-Output "Fileter info: ${audioFilterContent}"
+
+ffmpeg -i ${inputFile} `
+-codec:v hevc_nvenc -rc:v constqp -init_qpI 25 -init_qpP 29 -g 120 -fps_mode cfr -r 60 -multipass fullres -preset p7 -tune hq -profile:v main10 -tag:v hvc1 -level 5.2 `
+-codec:a libopus -b:a 160k -async 2 -filter:a ${audioFilterContent} `
+-hide_banner output-${inputFile}
+
+Remove-Item $loudnessInfo
