b515faa6b9639d2b5c66f00cccfa3a177a9e36a6
suke3 <35905884+suke3-gh@users.noreply.github.com>
2023-07-10 01:14:22 +0900

    ver.0.0.5

--- a-encode-to-opus.ps1
+++ a-encode-to-opus.ps1
@@ -2,13 +2,14 @@
 Param($format)
 foreach ($inputFile in Get-ChildItem -Filter *.${format} ) {
   $resultFileName = "loudness-${inputFile}.json"
+
+  Write-Host "Checking the loudness ... : ${inputFile}"
   ffmpeg -i ${inputFile} -hide_banner -filter:a loudnorm=I=-14:LRA=23:TP=-1:offset=0:print_format=json -vn -f null - 2> $resultFileName
-  Write-Host "Checking the loudness ... :${inputFile}"
   Get-Content $resultFileName -Tail 12 | Tee-Object -FilePath $resultFileName
 
   $jsonData = Get-Content -Raw $resultFileName | ConvertFrom-Json
   $filterContent = "loudnorm=I=-14:LRA=23:TP=-1:offset=0:measured_I=" + $jsonData.input_i + ":measured_TP=" + $jsonData.input_tp + ":measured_LRA=" + $jsonData.input_lra + ":measured_thresh=" + $jsonData.input_thresh + ":offset=" + $jsonData.target_offset + ":linear=false,anlmdn=s=0.00001:p=0.002:r=0.003"
-  Write-Host "filter info:" $filterContent
+  Write-Host "filter info : ${filterContent}"
 
   ffmpeg -i ${inputFile} -vn -codec:a libopus -b:a 160k -r:a 48k -filter:a $filterContent -hide_banner "${inputFile}".Replace(${format},'opus')
 
--- v-encode.ps1
+++ v-encode.ps1
@@ -3,15 +3,13 @@ Param($inputFile)
 
 $loudnessInfo = "loudness-${inputFile}.json"
 
-Write-Host "Checking the loudness..."
-
+Write-Host "Checking the loudness ... : ${inputFile}"
 ffmpeg -i ${inputFile} -hide_banner -filter:a loudnorm=I=-14:LRA=23:TP=-1:offset=0:print_format=json -vn -f null - 2> $loudnessInfo
-Write-Host "fileName: ${inputFile}"
 Get-Content $loudnessInfo -Tail 12 | Tee-Object -FilePath $loudnessInfo
 
 $jsonData = Get-Content -Raw $loudnessInfo | ConvertFrom-Json
 $audioFilterContent = "loudnorm=I=-14:LRA=23:TP=-1:offset=0:measured_I=" + $jsonData.input_i + ":measured_TP=" + $jsonData.input_tp + ":measured_LRA=" + $jsonData.input_lra + ":measured_thresh=" + $jsonData.input_thresh + ":offset=" + $jsonData.target_offset + ":linear=false,anlmdn=s=0.00001:p=0.002:r=0.002"
-Write-Output "Fileter info: ${audioFilterContent}"
+Write-Output "filter info : ${audioFilterContent}"
 
 ffmpeg -i ${inputFile} `
 -codec:v hevc_nvenc -rc:v constqp -init_qpI 25 -init_qpP 29 -g 120 -fps_mode cfr -r 60 -multipass fullres -preset p7 -tune hq -profile:v main10 -tag:v hvc1 -level 5.2 `
